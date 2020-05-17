c8c134371352de9d00f42b30850ea586f6a09872
Niasha Abaglo-Grant <niashagrant11@gmail.com>
2020-05-16 20:47:10 -0700

    removed the ORM and connection files, added the config.json file as well as filled out the local and heroku database information, lastly changed the models burger.js file so that it will work with sequelize documentation

--- models/burger.js
+++ models/burger.js
@@ -1,28 +1,15 @@
-var orm = require("../config/orm.js");
-
-// code that calls the ORM fucntions using specific input for the ORM.
-
-var burger = {
-  all: function (cb) {
-    orm.all("burger", function (res) {
-      cb(res);
-    });
-  },
-  create: function (cols, vals, cb) {
-    orm.create("burger", cols, vals, function (res) {
-      cb(res);
-    });
-  },
-  update: function (objColsVals, condition, cb) {
-    orm.update("burger", objColsVals, condition, function (res) {
-      cb(res);
-    });
-  },
-  delete: function (condition, cb) {
-    orm.delete("burger", condition, function (res) {
-      cb(res);
-    });
-  },
+module.exports = function (sequelize, DataTypes) {
+  var burger = sequelize.define("burger", {
+    nane: {
+      type: DataTypes.STRING,
+      validate: {
+        len: [1, 150],
+      },
+    },
+    devoured: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+  });
+  return burger;
 };
-
-module.exports = burger;
